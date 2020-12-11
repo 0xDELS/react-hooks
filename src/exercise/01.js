69ba4f61c8d4fa7bc9d0c49ea3bf5f4f93c14a21
Diogo Silva <imdiogosilva@gmail.com>
2020-12-11 12:46:19 +0000

    exercise 1 - extra credit 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,9 +3,9 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = React.useState('')
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
@@ -24,7 +24,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Diogo"/>
 }
 
 export default App
