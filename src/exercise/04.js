fb8a6e068795318d329068eb6d52902f8c7b9c88
Diogo Silva <imdiogosilva@gmail.com>
2020-12-17 22:34:13 +0000

    exercise 4 - useLocalStorage Custom Hook

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,21 +2,17 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = React.useState(
-    () =>
-      JSON.parse(window.localStorage.getItem('gameData')) ||
-      Array(9).fill(null),
+  const [squares, setSquares] = useLocalStorageState(
+    'squares',
+    Array(9).fill(null),
   )
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
-
-  React.useEffect(() => {
-    window.localStorage.setItem('gameData', JSON.stringify(squares))
-  })
 
   function selectSquare(square) {
     if (winner || squares[square] !== null) {
